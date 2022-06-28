d9686593e626c0f55f8a6b54d2044b70300c9929
Alexandr Sidoruk <sidoruk.alexandr8@gmail.com>
2022-06-28 23:59:18 +0300

    express auth,registration,login

---
+++ validations/auth.js
@@ -0,0 +1,10 @@
+import {body} from "express-validator";
+
+export const registerValidation = [
+    body('email', 'Неверный формат почты ').isEmail(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({min: 5}),
+    body('fullName', 'Имя должно быть минимум 3 символа').isLength({min: 3}),
+    body('avatarUrl', 'Не верная ссілка на аватарку').optional().isURL(),
+
+];
+
